69e7519ffc87d387089a5ca844a9e219cfbc2ac9
SpiralP <spiral.palmer@gmail.com>
2018-02-11 15:15:31 -0800

    curl: yes check certificates, don't show progress bar

--- scripts/download.pl
+++ scripts/download.pl
@@ -82,8 +82,8 @@ sub download_cmd($) {
 	}
 
 	return $have_curl
-		? (qw(curl -f --connect-timeout 20 --retry 5 --location --insecure), shellwords($ENV{CURL_OPTIONS} || ''), $url)
-		: (qw(wget --tries=5 --timeout=20 --no-check-certificate --output-document=-), shellwords($ENV{WGET_OPTIONS} || ''), $url)
+		? (qw(curl --fail --connect-timeout 20 --retry 5 --location --silent --show-error), shellwords($ENV{CURL_OPTIONS} || ''), $url)
+		: (qw(wget --tries=5 --timeout=20 --output-document=-), shellwords($ENV{WGET_OPTIONS} || ''), $url)
 	;
 }
 
